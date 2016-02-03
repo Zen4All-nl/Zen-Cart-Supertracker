eeea8fca104de6bffd6274c06bdb461b0151e3a6
Zen4All <info@zen4all.nl>
2016-02-03 13:45:08 +0100

    installer updates
    updates needed for version 1.2

--- 4_Sql files/uninstall.sql
+++ 4_Sql files/uninstall.sql
@@ -3,8 +3,8 @@ SELECT @configuration_group_id:=configuration_group_id
 FROM configuration_group
 WHERE configuration_group_title= 'Super Tracker'
 LIMIT 1;
-DELETE FROM configuration WHERE configuration_group_id = @configuration_group_id;
-DELETE FROM configuration_group WHERE configuration_group_id = @configuration_group_id;
+DELETE FROM configuration WHERE configuration_group_id = @configuration_group_id AND configuration_group_id != 0;
+DELETE FROM configuration_group WHERE configuration_group_id = @configuration_group_id AND configuration_group_id != 0;
 
 DROP TABLE IF EXISTS `supertracker`;
 
