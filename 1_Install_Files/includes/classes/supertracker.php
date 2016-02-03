08f0697a1c7e4f61fb5c7bfe105ca4d82c4e3263
Zen4All <info@zen4all.nl>
2016-02-03 14:21:21 +0100

    add city and region
    + code reformatting

--- 1_Install_Files/includes/classes/supertracker.php
+++ 1_Install_Files/includes/classes/supertracker.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @package supertracker
  * @copyright Copyright 2003-2006 Zen Cart Development Team
@@ -11,40 +12,39 @@
  * @version $Id: supertracker.php, v 1.0.0 09.05.2007 17:40 Andrew Berezin andrew@ecommerce-service.com $
  * @Security Fix - SQL Injection on Update (Line 172), Mathew Chan 25 Feb 2009
  */
-
 class supertracker {
 
-  function supertracker(){
+  function __construct() {
   }
 
   function update() {
     global $db, $cPath, $cPath_array, $breadcrumb;
 //// **** CONFIGURATION SECTION  **** ////
 
-    @define('XTRACKING_EXCLUDED_IPS', '');
-    @define('XTRACKING_EXCLUDED_UA', 'ServiceUptime.robot');
-    @define('XTRACKING_EXCLUDE_BOTS', 'true');
-
     if (XTRACKING_EXCLUDED_IPS != '') {
-      $ex_array = explode(',', str_replace(' ','', XTRACKING_EXCLUDED_IPS));
+      $ex_array = explode(',', str_replace(' ', '', XTRACKING_EXCLUDED_IPS));
       foreach ($ex_array as $key => $ex_ip) {
-        if ($_SERVER['REMOTE_ADDR'] == $ex_ip) return;
+        if ($_SERVER['REMOTE_ADDR'] == $ex_ip) {
+          return;
+        }
       }
     }
 
     if (XTRACKING_EXCLUDED_UA != '') {
       $ex_array = explode(',', XTRACKING_EXCLUDED_UA);
       foreach ($ex_array as $key => $ex_ua) {
-        if (strpos($_SERVER['HTTP_USER_AGENT'], trim($ex_ua)) !== false) return;
+        if (strpos($_SERVER['HTTP_USER_AGENT'], trim($ex_ua)) !== false) {
+          return;
+        }
       }
     }
 
-    if(!isset($spider_flag)) {
+    if (!isset($spider_flag)) {
       $user_agent = strtolower($_SERVER['HTTP_USER_AGENT']);
       $spider_flag = false;
       if (zen_not_null($user_agent)) {
         $spiders = file(DIR_WS_INCLUDES . 'spiders.txt');
-        for ($i=0, $n=sizeof($spiders); $i<$n; $i++) {
+        for ($i = 0, $n = sizeof($spiders); $i < $n; $i++) {
           if (zen_not_null($spiders[$i])) {
             if (is_integer(strpos($user_agent, trim($spiders[$i])))) {
               $spider_flag = true;
@@ -57,22 +57,23 @@ class supertracker {
       }
     }
 
-    if (XTRACKING_EXCLUDE_BOTS == 'true' && $spider_flag) return;
+    if (XTRACKING_EXCLUDE_BOTS == 'true' && $spider_flag) {
+      return;
+    }
 
     $user_agent = zen_db_prepare_input($_SERVER['HTTP_USER_AGENT']);
 
     $existing_session = false;
 
-    $thirty_mins_ago = date('Y-m-d H:i:s', (time() - (30*60)));
+    $thirty_mins_ago = date('Y-m-d H:i:s', (time() - (30 * 60)));
     //Find out if this user already appears in the supertracker db table
-
     //First thing to try is customer_id, if they are signed in
     if (isset($_SESSION['customer_id'])) {
       $tracking_data = $db->Execute("SELECT *
-                              FROM " . TABLE_SUPERTRACKER . "
-                              WHERE customer_id ='" . (int)$_SESSION['customer_id'] . "'
-                                AND last_click > '" . $thirty_mins_ago . "'
-                              LIMIT 1");
+                                     FROM " . TABLE_SUPERTRACKER . "
+                                     WHERE customer_id ='" . (int)$_SESSION['customer_id'] . "'
+                                     AND last_click > '" . $thirty_mins_ago . "'
+                                     LIMIT 1");
       if (!$tracking_data->EOF) {
         $existing_session = true;
       }
@@ -83,13 +84,13 @@ class supertracker {
     //that the chances of having more than 1 user within a 30 minute window with identical values
     //is pretty small, so hopefully this will work and should be more reliable than using Session IDs....
     if (!$existing_session) {
-      $ip_parts = explode ('.', $_SERVER['REMOTE_ADDR']);
+      $ip_parts = explode('.', $_SERVER['REMOTE_ADDR']);
       $tracking_data = $db->Execute("SELECT *
-                              FROM " . TABLE_SUPERTRACKER . "
-                              WHERE browser_string ='" . zen_db_input($user_agent) . "'
-                                AND ip_address like '" . $ip_parts[0] . '.' . $ip_parts[1] . "%'
-                                AND last_click > '" . $thirty_mins_ago . "'
-                              LIMIT 1");
+                                     FROM " . TABLE_SUPERTRACKER . "
+                                     WHERE browser_string ='" . zen_db_input($user_agent) . "'
+                                     AND ip_address like '" . $ip_parts[0] . '.' . $ip_parts[1] . "%'
+                                     AND last_click > '" . $thirty_mins_ago . "'
+                                     LIMIT 1");
       if (!$tracking_data->EOF) {
         $existing_session = true;
       }
@@ -98,12 +99,12 @@ class supertracker {
     //If that didn't work, and we have something in the cart, we can use that to try and find the
     //record instead. Obviously, people with things in their cart don't just appear from nowhere!
     if (!$existing_session) {
-      if ($_SESSION['cart']->count_contents()>0) {
+      if ($_SESSION['cart']->count_contents() > 0) {
         $tracking_data = $db->Execute("SELECT *
-                                FROM " . TABLE_SUPERTRACKER . "
-                                WHERE cart_total ='" . $_SESSION['cart']->show_total() . "'
-                                  AND last_click > '" . $thirty_mins_ago . "'
-                                LIMIT 1");
+                                       FROM " . TABLE_SUPERTRACKER . "
+                                       WHERE cart_total ='" . $_SESSION['cart']->show_total() . "'
+                                       AND last_click > '" . $thirty_mins_ago . "'
+                                       LIMIT 1");
         if (!$tracking_data->EOF) {
           $existing_session = true;
         }
@@ -118,13 +119,15 @@ class supertracker {
       $cart_contents = unserialize($tracking_data->fields['cart_contents']);
       $cart_total = $tracking_data->fields['cart_total'];
       $order_id = $tracking_data->fields['order_id'];
-      if (isset($_SESSION['customer_id']))
+      if (isset($_SESSION['customer_id'])) {
         $customer_id = (int)$_SESSION['customer_id'];
-      else
+      } else {
         $customer_id = (int)$tracking_data->fields['customer_id'];
-
+      }
       //Find out if the customer has added something to their cart for the first time
-      if ($_SESSION['cart']->count_contents() > 0) $tracking_data->fields['added_cart'] = 'true';
+      if ($_SESSION['cart']->count_contents() > 0) {
+        $tracking_data->fields['added_cart'] = 'true';
+      }
 
       //Has a purchase just been completed?
       if (($_GET['main_page'] == FILENAME_CHECKOUT_SUCCESS) && ($completed_purchase != 'true')) {
@@ -160,8 +163,8 @@ class supertracker {
       if ($_GET['main_page'] == FILENAME_DEFAULT) {
         if (isset($_GET['cPath'])) {
           $cat_id = zen_db_prepare_input($_GET['cPath']);
-          $cat_id_array = explode('_',$cat_id);
-          $cat_id = $cat_id_array[sizeof($cat_id_array)-1];
+          $cat_id_array = explode('_', $cat_id);
+          $cat_id = $cat_id_array[sizeof($cat_id_array) - 1];
           $categories_viewed[$cat_id] = 1;
         }
       }
@@ -184,19 +187,14 @@ class supertracker {
                         cart_total = '" . zen_db_input($cart_total) . "',
                         order_id = '" . (int)$order_id . "'
                     WHERE tracking_id='" . (int)$tracking_data->fields['tracking_id'] . "'");
-          
-      // SQL Injection Security Fix (Mathew Chan, 25 Feb 2009)
-      // $db->prepareInput(zen_db_prepare_input(urldecode($_SERVER['REQUEST_URI'])))
-      
-      //$db->Execute("UPDATE " . TABLE_SUPERTRACKER . " SET last_click = NOW(), exit_page='" . zen_db_prepare_input(urldecode($_SERVER['REQUEST_URI'])) . "', exit_page_name='" . $page_title . "', num_clicks=num_clicks+1, added_cart='" . $tracking_data->fields['added_cart'] . "', categories_viewed='" . $categories_viewed . "', products_viewed='" . $tracking_data->fields['products_viewed'] . "', customer_id='" . $customer_id . "', completed_purchase='" . $completed_purchase . "', cart_contents='" . $cart_contents . "', cart_total = '" . $cart_total . "', order_id = '" . $order_id . "' WHERE tracking_id='" . $tracking_data->fields['tracking_id'] . "'");
 
     } else {
       //New vistor, so record referrer, etc
       //Next line defines pages on which a new visitor should definitely not be recorded
       $prohibited_pages = 'login,checkout_shipping,checkout_payment,checkout_process,checkout_confirmation,checkout_success';
-      $prohibited_pages = '';
+//      $prohibited_pages = '';
       if (!strpos($prohibited_pages, $_GET['main_page'])) {
-        if(isset($_SERVER['HTTP_REFERER'])) {
+        if (isset($_SERVER['HTTP_REFERER'])) {
           $refer_data = zen_db_prepare_input(urldecode($_SERVER['HTTP_REFERER']));
           $refer_url_array = parse_url($refer_data);
           $referrer = $refer_url_array['scheme'] . '://' . $refer_url_array['host'] . $refer_url_array['path'];
@@ -206,10 +204,15 @@ class supertracker {
           $referrer_query_string = '';
         }
 
-        if(is_file(DIR_WS_INCLUDES . "GeoIP.dat")) {
-          include(DIR_WS_INCLUDES . "geoip.inc");
-          $gi = geoip_open(DIR_WS_INCLUDES . "GeoIP.dat", GEOIP_STANDARD);
-          $country_code = strtolower(geoip_country_code_by_addr($gi, $_SERVER['REMOTE_ADDR']));
+        if(is_file(DIR_WS_INCLUDES . "GeoLiteCity.dat")) {
+          include(DIR_WS_INCLUDES . "geoipcity.inc");
+          $gi = geoip_open(DIR_WS_INCLUDES . "GeoLiteCity.dat", GEOIP_STANDARD);
+          $record = geoip_record_by_addr($gi, $_SERVER['REMOTE_ADDR']);
+          $country_name = $record->country_name;
+          $country_code = strtolower($record->country_code);
+          $region_name = $GEOIP_REGION_NAME[$record->country_code][$record->region];
+          $city_name = $record->city;
+//          $country_code = strtolower(geoip_country_code_by_addr($gi, $_SERVER['REMOTE_ADDR']));
           geoip_close($gi);
         } else {
           $country_code = '--';
@@ -228,13 +231,15 @@ class supertracker {
           }
         }
 
-        $db->Execute("INSERT INTO " . TABLE_SUPERTRACKER . " (ip_address, browser_string, country_code, referrer, referrer_query_string, landing_page, landing_page_name, exit_page, exit_page_name, time_arrived, last_click, products_viewed) VALUES ('" . zen_db_input($_SERVER['REMOTE_ADDR']) . "', '" . zen_db_input($user_agent) . "', '" . $country_code . "', '" . zen_db_input($referrer) . "', '" . zen_db_input($referrer_query_string) . "', '" . $this_page . "', '" . $page_title . "', '" . $this_page . "', '" . $page_title . "', NOW(), NOW(), '" . zen_db_input($products_viewed) . "')");
+        $query = "INSERT INTO " . TABLE_SUPERTRACKER . " (ip_address, browser_string, country_code,country_name,country_region,country_city, referrer, referrer_query_string, landing_page, landing_page_name, exit_page, exit_page_name, time_arrived, last_click, products_viewed)
+                  VALUES ('" . zen_db_input($_SERVER['REMOTE_ADDR']) . "', '" . zen_db_input($user_agent) . "', '" . zen_db_input($country_code) . "', '" . zen_db_input($country_name) . "', '" . zen_db_input($region_name) . "', '" . zen_db_input($city_name) . "', '" . zen_db_input($referrer) . "', '" . zen_db_input($referrer_query_string) . "', '" . $this_page . "', '" . $page_title . "', '" . $this_page . "', '" . $page_title . "', NOW(), NOW(), '" . zen_db_input($products_viewed) . "')";
+        $db->Execute($query);
 
       }//end if for prohibited pages
     }//end else
+  }
 
-  }//End function update
+//End function update
+}
 
-}//End Class
-
-?>
+//End Class
