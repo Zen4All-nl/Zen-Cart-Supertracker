64ddce9e0ffa12877e239fe1d9a81f10cfff8a9b
Zen4All <info@zen4all.nl>
2016-02-03 13:44:18 +0100

    updated for admin rights
    Updated files so it plays naice with the admin rights in Zen Cart

--- 1_Install_Files/YOUR_ADMIN/includes/extra_datafiles/supertracker.php
+++ 1_Install_Files/YOUR_ADMIN/includes/extra_datafiles/supertracker.php
@@ -1,5 +1,5 @@
 <?php
   define('BOX_CONFIGURATION_SUPERTRACKER','Super Tracker Configuration');
   define('TABLE_SUPERTRACKER', DB_PREFIX . 'supertracker');
-  define('FILENAME_SUPERTRACKER', 'supertracker.php');
+  define('FILENAME_SUPERTRACKER', 'supertracker');
   define('DIR_WS_FLAGS', 'images/flags/');
